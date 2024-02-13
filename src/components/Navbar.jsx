c2918191579c8eb5fdff3bae2f7805523ec60c8e
workwithraymond <workwithraymond01@gmail.com>
2024-02-12 19:04:46 -0500

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import WhatsApp from './WhatsApp';
 import PageLinks from './PageLinks';
-import NewMenu from './NewMenu';
 
 
 
@@ -12,20 +11,19 @@ const Navbar = () => {
         <nav className="navbar">
           <img src="./image/logo.jpg" alt="Restaurant Logo" className="nav-logo" />
  
-        
-        <NewMenu/>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="hamburger-menu">
        
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         
         <PageLinks parentClass='nav-links' itemClass='nav-link'/>
         <ul id="nav-links" className="nav-links">
-        
+       
           <WhatsApp />      
           
         </ul>
-        
       </nav>
       )
 
